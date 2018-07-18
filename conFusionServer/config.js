ef4704d13169cd018e8daeb991ee05eb758f07b7
sweezy793 <sarthaks793@gmail.com>
2018-07-19 01:14:08 +0530

    OAuth using Passport Facebook token

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports={
     'secretKey':'12345-67890-09876-54321',
-    'mongoUrl':'mongodb://localhost:27017/conFusion'
+    'mongoUrl':'mongodb://localhost:27017/conFusion',
+    'facebook':{
+        clientId:'234832727133288',
+        clientSecret:'ce1c8f7d8bc7fd57a84c78b14a77ad9b'
+    }
 }
--- conFusionServer/routes/users.js
+++ conFusionServer/routes/users.js
@@ -54,7 +54,7 @@ router.post('/signup', cors.corsWithOptions,(req, res, next) => {
 });
 
 router.post('/login', cors.corsWithOptions,passport.authenticate('local'),(req, res, next) => {
-        var token=authenticate.getToken({_id:req.user._id})
+        var token=authenticate.getToken({_id:req.user._id});
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
         res.json({success:true,token:token,status: 'You are logged in'});
@@ -72,5 +72,16 @@ router.get('/logout',cors.corsWithOptions, (req, res,next) => {
     next(err);
   }
 });
+
+router.get('/facebook/token',passport.authenticate('facebook-token'),(req,res)=>{
+    if(req.user)
+    {
+      var token=authenticate.getToken({_id:req.user._id});
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.json({success:true,token:token,status: 'You are logged in'});
+
+    }
+})
 
 module.exports = router;
