dbc2f03678c9b2d6c81a3ba7357bff9c9a548515
sweezy793 <sarthaks793@gmail.com>
2018-07-30 23:03:27 +0530

    Can't undertand why the comments won't show

--- yelp-camp/app.js
+++ yelp-camp/app.js
@@ -4,7 +4,11 @@ var http=require('http');
 var bodyParser=require("body-parser");
 var mongoose=require('mongoose');
 var Campground=require('./models/campground');
+var Comment=require('./models/comment');
+var seedDB=require('./seeds');
 
+
+seedDB();
 
 mongoose.connect('mongodb://localhost:27017/yelp_camp', { useNewUrlParser: true });
 
@@ -40,7 +44,7 @@ app.get("/campgrounds",(req,res)=>{
             console.log(err);
         }
         else{
-            res.render("index",{campgrounds:allCampgrounds});
+            res.render("campgrounds/index",{campgrounds:allCampgrounds});
         }
     });
     
@@ -60,19 +64,55 @@ app.post("/campgrounds",(req,res)=>{
 });
 
 app.get("/campgrounds/new",(req,res)=>{
-    res.render("new.ejs");
+    res.render("campgrounds/new");
 });
 
-app.get("/campgrounds/:id",(req,res)=>{
-    Campground.findById(req.params.id,(err,foundCampground)=>{
-        if(err)
+app.get("/campgrounds/:id", function(req, res){
+    //find the campground with provided ID
+    Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
+        if(err){
             console.log(err);
-        else
-        {
-            res.render("show",{campground:foundCampground});
+        } else {
+            console.log(foundCampground)
+            //render show template with that campground
+            res.render("campgrounds/show", {campground: foundCampground});
         }
     });
 });
+
+app.get("/campgrounds/:id/comments/new", function(req, res){
+    // find campground by id
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+        } else {
+             res.render("comments/new", {campground: campground});
+        }
+    })
+});
+
+app.post("/campgrounds/:id/comments", function(req, res){
+    //lookup campground using ID
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+            res.redirect("/campgrounds");
+        } else {
+         Comment.create(req.body.comment, function(err, comment){
+            if(err){
+                console.log(err);
+            } else {
+                campground.comments.push(comment);
+                campground.save();
+                res.redirect('/campgrounds/' + campground._id);
+            }
+         });
+        }
+    });
+    //create new comment
+    //connect new comment to campground
+    //redirect campground show page
+ });
 
 http.createServer(app).listen("3000",(req,res)=>{
     console.log("Server is running");
