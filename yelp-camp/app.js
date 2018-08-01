494fe9861388224365a23cafadb0e4aa4530bb81
sweezy793 <sarthaks793@gmail.com>
2018-08-01 23:55:58 +0530

    Added dependencies and configs for auth

--- yelp-camp/app.js
+++ yelp-camp/app.js
@@ -3,6 +3,9 @@ var app=express();
 var http=require('http');
 var bodyParser=require("body-parser");
 var mongoose=require('mongoose');
+var passport=require('passport');
+var LocalStrategy=require('passport-local');
+var User=require('./models/user');
 var Campground=require('./models/campground');
 var Comment=require('./models/comment');
 var seedDB=require('./seeds');
@@ -13,6 +16,10 @@ seedDB();
 mongoose.connect('mongodb://localhost:27017/yelp_camp', { useNewUrlParser: true });
 
 app.use(bodyParser.urlencoded({extended:true}));
+
+app.set("view engine","ejs");
+
+app.use(express.static(__dirname+"/public"));
 
 
 
@@ -31,7 +38,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 //     console.log(campground);
 // });
 
-app.set("view engine","ejs");
+
 
 app.get("/",(req,res)=>{
     res.render("landing");
