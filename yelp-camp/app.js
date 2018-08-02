a154c251bc7639e908033e440a1448ff9225fe7f
sweezy793 <sarthaks793@gmail.com>
2018-08-02 23:48:31 +0530

    nav items change according to auth

--- yelp-camp/app.js
+++ yelp-camp/app.js
@@ -35,6 +35,12 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+app.use(function(req,res,next)
+{
+    res.locals.currentUser=req.user;
+    next();
+})
+
 //Schema
 
 
@@ -62,7 +68,7 @@ app.get("/campgrounds",(req,res)=>{
             console.log(err);
         }
         else{
-            res.render("campgrounds/index",{campgrounds:allCampgrounds});
+            res.render("campgrounds/index",{campgrounds:allCampgrounds,currentUser:req.user});
         }
     });
     
