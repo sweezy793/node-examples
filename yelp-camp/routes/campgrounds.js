7b5f5d00993faee79e59b83873ae2d09e64bcd0f
sweezy793 <sarthaks793@gmail.com>
2018-08-03 13:58:15 +0530

    only logged in user can add campgrounds now

--- yelp-camp/routes/campgrounds.js
+++ yelp-camp/routes/campgrounds.js
@@ -16,7 +16,7 @@ router.get("/",(req,res)=>{
     
 });
 
-router.post("/",(req,res)=>{
+router.post("/",isLoggedIn,(req,res)=>{
     var name=req.body.name;
     var image=req.body.image;
     var desc=req.body.description;
@@ -29,7 +29,7 @@ router.post("/",(req,res)=>{
     });   
 });
 
-router.get("/new",(req,res)=>{
+router.get("/new",isLoggedIn,(req,res)=>{
     res.render("campgrounds/new");
 });
 
