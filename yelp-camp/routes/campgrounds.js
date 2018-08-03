a16b041675e15ec511c147c6c38ce62397fd7345
sweezy793 <sarthaks793@gmail.com>
2018-08-03 14:18:39 +0530

    some cosmetic changes

--- yelp-camp/routes/campgrounds.js
+++ yelp-camp/routes/campgrounds.js
@@ -20,7 +20,11 @@ router.post("/",isLoggedIn,(req,res)=>{
     var name=req.body.name;
     var image=req.body.image;
     var desc=req.body.description;
-    var newCampground={name:name,image:image,description:desc};
+    var author={
+        id:req.user._id,
+        username:req.user.username
+    };
+    var newCampground={name:name,image:image,description:desc,author,author};
     Campground.create(newCampground,(err,newlyCreated)=>{
         if(err)
         console.log(err);
